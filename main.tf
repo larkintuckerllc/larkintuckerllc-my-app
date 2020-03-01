827e0686013fc54f3491d97d668ef605d9e58b2a
John Tucker <john@larkintuckerllc.com>
2020-03-01 15:43:00 -0500

    deploy app

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "heroku_app" "larkintuckerllc-my-app" {
   name   = "larkintuckerllc-my-app"
   region = "us"
 }
+
+resource "heroku_build" "larkintuckerllc-my-app_build" {
+  app = heroku_app.larkintuckerllc-my-app.id
+
+  source = {
+    path = "."
+  }
+}
